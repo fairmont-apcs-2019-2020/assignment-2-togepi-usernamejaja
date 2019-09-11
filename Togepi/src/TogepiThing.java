efa65aaac4164803156188d476d867f0e0299ae6
jacobburnley <jacobburnley@10.2.4.251>
2019-09-10 18:54:41 -0700

    WOO HOO!

--- Togepi/src/TogepiThing.java
+++ Togepi/src/TogepiThing.java
@@ -1,14 +1,10 @@
 
 public class TogepiThing extends PokeThing {
-
-	public TogepiThing()
-	{
+	public TogepiThing() {
 		super("togepi");
 	}
-	
-	public void step()
-	{
-		move(null);
+	public void step() {
+		move();
 	}
-	
 }
+
